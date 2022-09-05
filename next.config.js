8efff39bbae42119d404ed6981ce32aa70598fde
Breno Souza <cognocoder@gmail.com>
2022-09-04 23:50:14 -0300

    chore: absolute imports, dependencies

--- next.config.js
+++ next.config.js
@@ -4,4 +4,11 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+const compiler = {
+  styledComponents: {
+    displayName: true,
+    ssr: true,
+  },
+}
+
+module.exports = { ...nextConfig, compiler }
